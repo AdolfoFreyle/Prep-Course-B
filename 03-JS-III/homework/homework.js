227f599c44d908520f18d25aece16627bd2b73bc
AdolfoFreyle <adolfo.freyle@gmail.com>
2022-11-02 09:02:44 -0500

    Test III

--- 03-JS-III/homework/homework.js
+++ 03-JS-III/homework/homework.js
@@ -8,29 +8,52 @@ function imprimirSumaNumeros() {
     // Imprime la suma de los números del 1 al 10
     // Pista: usa un acumulador
     // Tu código:
+    var suma=0;
+    for(let i=0 ; i < 10 ; i++){
+      suma= suma + i
+    }
 }
 
 function encuentraPares(array){
   // Devuelve un arreglo con los pares encontrados
   // en el arreglo de enteros pasado como parámetro
   // Tu código:
+
+  var pares= array.filter((i)=> i%2==0);
+  console.log(pares)
 }
 
 function elevaAlCuadrado(array){
   // Devuelve un arreglo con cada número del array
   // elevado al cuadrado
   // Tu código:
-}
+  var nuevoArray=[]
+   for (let i =0; i < array.length; i++) {
+    console.log(nuevoArray[i]= array[i]* array[i]);
+    
+  }
+  }
+
 
 function sumaArray(array){
   // Devuelve el resultado de sumar todos los elementos
   // de un arreglo de enteros dado
   // Tu código:
+var suma=0;
+ for (let i = 0; i< array.length; i++) {
+  suma = suma + array[i]
+  }
 }
 
 function numeroDigitos(num){
   // Devuelve el número de dígitos de un número dado
   // Tu código:
+  var contador=0
+while(num >=1){
+   contador= contador +1
+  num/=10
+}
+console.log (contador)
 }
   
   
